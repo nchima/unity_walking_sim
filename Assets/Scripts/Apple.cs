c62f4af1d1db9b0944bce336cbe9731b7938c7d1
Nchima Kapoma <kapomanchima@yahoo.com>
2020-10-25 18:39:38 +0200

    We have world teleportation. Finally!!!

---
+++ Assets/Scripts/Apple.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+
+    PlayerController playerController;
+    GameObject playerMain;
+    GameObject playerAlt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+
+        playerMain = playerController.GetPlayerMain();
+        playerAlt = playerController.GetPlayerAlt();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //var playerHoldingApple = playerAlt.GetComponentInChildren<Apple>();
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Debug.Log("Collided with apple");
+
+        if (other.tag == "player-main")
+        {
+            transform.parent = playerAlt.transform;
+            transform.localPosition = new Vector3(0, 0, 2);
+
+            playerController.SendToApocalypticWorld();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    GameObject playerMain;
+    GameObject playerAlt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var players = FindObjectsOfType<Player>();
+
+        foreach (var player in players)
+        {
+
+            if (player.gameObject.tag == "player-main")
+            {
+                playerMain = player.gameObject;
+            }
+
+            if (player.gameObject.tag == "player-ruins")
+            {
+                playerAlt = player.gameObject;
+            }
+
+
+
+        }
+
+        // Deactivate none active player
+        playerMain.GetComponentInChildren<Camera>().enabled = true;
+        playerAlt.GetComponentInChildren<Camera>().enabled = false;
+
+    }
+
+    private void Update()
+    {
+        playerAlt.transform.localPosition = playerMain.transform.localPosition;
+
+        if (playerAlt.GetComponentInChildren<Camera>().isActiveAndEnabled == true)
+        {
+
+        }
+
+
+
+        //Debug.Log(playerAlt.transform.localPosition + "------" + playerMain.transform.localPosition);
+        //playerAlt.transform.position = playerMain.transform.position;
+        //playerAlt.transform.rotation = playerMain.transform.rotation;
+
+    }
+
+    public GameObject GetPlayerMain()
+    {
+        return playerMain;
+    }
+
+    public GameObject GetPlayerAlt()
+    {
+        return playerAlt;
+    }
+
+    public void SendToApocalypticWorld()
+    {
+
+        playerMain.GetComponentInChildren<Camera>().enabled = false;
+        playerAlt.GetComponentInChildren<Camera>().enabled = true;
+    }
+
+    public void SendToNormalWorld()
+    {
+
+        playerAlt.GetComponentInChildren<Camera>().enabled = false;
+        playerMain.GetComponentInChildren<Camera>().enabled = true;
+
+    }
+}
